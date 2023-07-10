a51c7337831cd12627d4681371006d8a8324aeee
siddyamgond <117370082+siddyamgond@users.noreply.github.com>
2023-07-10 15:26:50 +0530

    Create Swap1.java

---
+++ Core java/Swap1.java
@@ -0,0 +1,12 @@
+
+
+class Test {
+    public static void main(String[] args) {
+       int a=1,b=2,temp=0;
+       System.out.println("Before swap "+a+" "+b);
+       temp=a;
+       a=b;
+       b=temp;
+       System.out.println("After swap "+a+" "+b);
+    }
+}
